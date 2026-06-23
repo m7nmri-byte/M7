a3c8c95a64e71c03f105e0f22eb65322eb88ee43
godisgreat621-sys <god.is.great.621@gmail.com>
2026-06-23 16:34:19 +0300

    fix: no duplicate sources in people list, mobile add buttons for policies/plans, plan detail mobile layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2788236966" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1228302002" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
