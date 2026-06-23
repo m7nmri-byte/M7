d8e4348a33dc2c40530b509e660f2cd06d2bd10b
godisgreat621-sys <god.is.great.621@gmail.com>
2026-06-23 05:24:20 +0300

    feat: person linked to sources, source picked first in task form, people sorted by source

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2081129310" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4238155902" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
